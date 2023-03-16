dd8425ff5b859fd113436acfa697326fdca00f65
Giuseppe-Andinolfi <122208915+Giuseppe-Andinolfi@users.noreply.github.com>
2023-03-16 16:19:35 +0100

    Update TrainingManagementApplication.java

--- src/main/java/com/yourSystem/project/TrainingManagementApplication.java
+++ src/main/java/com/yourSystem/project/TrainingManagementApplication.java
@@ -1,7 +1,7 @@
 package com.yourSystem.project;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 public class TrainingManagementApplication {
 
