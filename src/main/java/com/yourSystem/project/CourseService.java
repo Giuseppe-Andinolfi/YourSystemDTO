b116a52b35253c8e49fa4750b52d6241418937af
Giuseppe-Andinolfi <122208915+Giuseppe-Andinolfi@users.noreply.github.com>
2023-03-16 16:19:17 +0100

    Update CourseService.java

--- src/main/java/com/yourSystem/project/CourseService.java
+++ src/main/java/com/yourSystem/project/CourseService.java
@@ -1,71 +1,96 @@
 package com.yourSystem.project;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.io.IOException;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
-
-import com.yourSystem.project.Course;
 
 @Service
 public class CourseService {
-
     private final CourseRepository courseRepository;
 
     public CourseService(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
     }
 
-    public Course createCourse(Course course) {
+    public CourseDTO createCourse(CourseDTO course) {
         return courseRepository.save(course);
     }
 
-    public Course getCourseById(Long id) {
-        return courseRepository.findById(id).orElse(null);
+    public CourseDTO mapToCourseDTO(CourseDTO course) {
+        CourseDTO courseDTO = new CourseDTO();
+        courseDTO.setId(course.getId());
+        courseDTO.setName(course.getName());
+        courseDTO.setContent(course.getContent());
+        courseDTO.setMaxNumberOfParticipants(course.getMaxNumberOfParticipants());
+        courseDTO.setUrl(course.getUrl());
+        return courseDTO;
     }
 
-    public List<Course> getAllCourses() {
-        return courseRepository.findAll();
+    public CourseDTO mapToCourse(CourseDTO courseDTO) {
+        CourseDTO course = new CourseDTO();
+        course.setId(courseDTO.getId());
+        course.setName(courseDTO.getName());
+        course.setContent(courseDTO.getContent());
+        course.setMaxNumberOfParticipants(courseDTO.getMaxNumberOfParticipants());
+        course.setUrl(courseDTO.getUrl());
+        return course;
     }
 
-    public Course updateCourse(Long id, Course course) {
-        Course existingCourse = courseRepository.findById(id).orElse(null);
+    public CourseDTO getCourseById(Long id) {
+        Optional<CourseDTO> courseOptional = courseRepository.findById(id);
+        CourseDTO course = courseOptional.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found"));
+        return mapToCourseDTO(course);
+    }
+
+    public List<CourseDTO> getAllCourses() {
+        List<CourseDTO> courses = courseRepository.findAll();
+        return courses.stream().map(this::mapToCourseDTO).collect(Collectors.toList());
+    }
+
+    public CourseDTO updateCourse(Long id, CourseDTO courseDTO) {
+        CourseDTO existingCourse = courseRepository.findById(id).orElse(null);
         if (existingCourse == null) {
             return null;
         } else {
+            CourseDTO course = mapToCourse(courseDTO);
             course.setId(id);
-            return courseRepository.save(course);
+            course = courseRepository.save(course);
+            return mapToCourseDTO(course);
         }
     }
 
-    public void deleteCourse(Long id) {
+    public void deleteCourseDTO(Long id) {
         courseRepository.deleteById(id);
+
     }
 
-    public void exportToExcel() throws IOException {
-        List<Course> courses = courseRepository.findAll();
-
-        // Create a new Excel workbook
+    public void exportToExcel(String filePath) throws IOException {
+        List<CourseDTO> courses = courseRepository.findAll();
+        // create a new Excel workbook and sheet
         Workbook workbook = new XSSFWorkbook();
-
-        // Create a new sheet
         Sheet sheet = workbook.createSheet("Courses");
-
-        // Create a row for the table header
+        // create a header row for the sheet
         Row headerRow = sheet.createRow(0);
-        headerRow.createCell(0).setCellValue("ID");
+        headerRow.createCell(0).setCellValue("Id");
         headerRow.createCell(1).setCellValue("Name");
         headerRow.createCell(2).setCellValue("Content");
-        headerRow.createCell(3).setCellValue("Maximum number of participants");
+        headerRow.createCell(3).setCellValue("Max Number of Participants");
         headerRow.createCell(4).setCellValue("URL");
-
-        // Iterate over all the courses and create a row for each of them
+        // add course data to the sheet
         int rowNum = 1;
-        for (Course course : courses) {
+        for (CourseDTO course : courses) {
             Row row = sheet.createRow(rowNum++);
             row.createCell(0).setCellValue(course.getId());
             row.createCell(1).setCellValue(course.getName());
@@ -73,10 +98,10 @@ public class CourseService {
             row.createCell(3).setCellValue(course.getMaxNumberOfParticipants());
             row.createCell(4).setCellValue(course.getUrl());
         }
-
-        // Write the workbook to an Excel file
-        FileOutputStream fileOut = new FileOutputStream("courses.xlsx");
-        workbook.write(fileOut);
-        fileOut.close();
+        // write the workbook to a file
+        OutputStream outputStream = new FileOutputStream(filePath);
+        workbook.write(outputStream);
+        outputStream.close();
     }
+
 }
